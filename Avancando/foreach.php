0c7077e7c5eceebf309d2f7c6cc4c101eb8fe460
Franklinmateus <frankmateus660@gmail.com>
2022-09-05 16:16:23 -0300

    Mudando de nome

--- Avancando/foreach.php
+++ Avancando/foreach.php
@@ -1,21 +1,25 @@
 <?php
 
 $contasCorrentes = [
-    872346837 => [
+    '872.346.837-23' => [
         'titular' => 'Franklin',
         'saldo' => 1000
     ], 
-    289345738 => [
+    '289.345.738-45' => [
         'titular' => 'Marlon',
         'saldo' => 2000
     ], 
-    123344555 => [
+    '123.344.555-67' => [
         'titular' =>'Mariana',
         'saldo' => 100
     ]
-    ];
+];
+
+$contasCorrentes['888.344.555-68'] = [
+    'titular' => 'Ana',
+    'saldo' => 0
+];
 
 foreach($contasCorrentes as $cpf => $conta){
-    echo $cpf . PHP_EOL;
-    echo $conta['titular'] . PHP_EOL;
+    echo $cpf ." ". $conta['titular'] . PHP_EOL;
 }
--- Avancando/listas.php
+++ Avancando/listas.php
@@ -3,6 +3,6 @@
 $idadeList = [21, 24, 25, 18, 17, 19, 23];
 
 
-for($i = 0; $i < count($idadeList); $i++){
-    echo $idadeList[$i] . PHP_EOL;
+foreach ($idadeList as $idade){
+    echo $idade . PHP_EOL;
 }

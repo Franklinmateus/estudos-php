9477a3b48ae64e48a89169c707d309376e5e91d2
Franklinmateus <frankmateus660@gmail.com>
2022-09-22 09:33:49 -0300

    Arquivos separados

--- Introducao - part2/banco.php
+++ Introducao - part2/banco.php
@@ -1,28 +1,6 @@
 <?php
 
-function exibeMensagem(string $mensagem)
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function sacar(array $conta, float $valor) : array
-{
-    if($valor > $conta['saldo']){
-        exibeMensagem("Você não pode sacar."); 
-    }else{
-        $conta['saldo'] -= $valor;
-    }
-
-    return $conta;
-}
-
-function depositar(array $conta, float $valor){
-    if($valor > 0){
-        $conta['saldo'] += $valor;
-    }else{
-        exibeMensagem("error");
-    }
-}
+require_once 'funcoes.php';
 
 $contasCorrentes = [
     '872.346.837-23' => [
@@ -40,11 +18,9 @@ $contasCorrentes = [
 ];
 
 $contasCorrentes['289.345.738-45'] = sacar($contasCorrentes['289.345.738-45'], 500);
-$contasCorrentes['123.344.555-67'] = sacar($contasCorrentes['123.344.555-67'], 500);
-
-$contasCorrentes['872.346.837-23'] = depositar($contasCorrentes['872.346.837-23'], -1100);
-
 
 foreach($contasCorrentes as $cpf => $conta){
-    exibeMensagem($cpf ." ". $conta['titular'] ." ". $conta['saldo']);
+    exibeMensagem(
+        "$cpf {$conta['titular']} {$conta['saldo']}"
+    );
 }
---
+++ Introducao - part2/funcoes.php
@@ -0,0 +1,26 @@
+<?php
+
+function exibeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+function sacar(array $conta, float $valor) : array
+{
+    if($valor > $conta['saldo']){
+        exibeMensagem("Você não pode sacar."); 
+    }else{
+        $conta['saldo'] -= $valor;
+    }
+
+    return $conta;
+}
+
+function depositar(array $conta, float $valor)
+{
+    if($valor > 0){
+        $conta['saldo'] += $valor;
+    }else{
+        exibeMensagem("error");
+    }
+}
